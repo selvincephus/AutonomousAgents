13af38e023b6828ba622e74a498f3ad28151017d
Alireza Tashvir <alireza.tashvir@gmail.com>
2015-04-10 16:48:58 +0200

    New behavior for the robot

--- sketch_apr01a/sketch_apr01a.ino
+++ sketch_apr01a/sketch_apr01a.ino
@@ -5,6 +5,9 @@
 Servo servoLeft;
 Servo servoRight;
 int stayThreshold = 500; //it can be translated to 1000 milisecond maybe
+int leftCount = 0;
+int rightCount = 0;
+float lightThreshold = 0.1;
 
 void setup()
 {
@@ -15,6 +18,8 @@ void setup()
   servoLeft.writeMicroseconds(1500);
   servoRight.writeMicroseconds(1500);
   Serial. begin(9600) ;
+  delay(100);
+  lightThreshold = volts(A3) * 0.75;
 }
 
 void loop() {
@@ -24,30 +29,68 @@ void loop() {
     Serial. print("Left");
     Serial. println(irLeft) ; // Display 1/0 no detect/detect
     
+    // read the analog value for left phototransistor
+    float leftPhoto = volts(A3);
+    Serial.println(leftPhoto);
+    // read the analog value for right phototransistor
+    float rightPhoto = 0.24;
+    if (leftPhoto < lightThreshold && rightPhoto > lightThreshold){
+      foundMine(String("l"));
+    }
+    else if (leftPhoto > lightThreshold && rightPhoto < lightThreshold){
+      foundMine(String("r"));
+    }
+    else if (leftPhoto < lightThreshold && rightPhoto < lightThreshold){
+      // means both found mines
+      foundMine(String("b"));
+    }
 //    int irRight = irDetect(5, 2, 38000) ; // Check for obj ect
     int irRight = irReading(String("r"));
     Serial. print("Right");
     Serial. println(irRight) ; // Display 1/0 no detect/detect
     delay(100) ;
     int counter = 0;
-    if (irLeft == 0 && irRight == 1)
+    if (irLeft == 1)
+    {
+      leftCount = 0;
+    }
+    else
+    {
+        leftCount++;
+    }
+    if (irRight == 1)
+    {
+      rightCount = 0;
+    }
+    else
+    {
+      rightCount++;
+    }
+//    if (irLeft == 0 && irRight == 1)
+    if (leftCount >= 5 && rightCount < 5)
     {
       while (irLeft == 0 && counter < stayThreshold) {
         turnRight();
         irLeft = irReading(String("l"));
         counter = counter + 1;
+        leftCount = 0;
+        rightCount = 0;
 //        Serial. println("Turning Right");
       }
     }
-    else if (irRight == 0 && irLeft == 1)
+//    else if (irRight == 0 && irLeft == 1)
+    else if (rightCount >= 5 && leftCount < 5)
     {
       while (irRight == 0 && counter < stayThreshold) {
         turnLeft();
         irRight = irReading(String("r"));
         counter = counter + 1;
+        leftCount = 0;
+        rightCount = 0;
       }
     }
-    else if (irLeft == 0 && irRight == 0)
+//    else if (irLeft == 0 && irRight == 0)
+    else if (leftCount >= 5 && rightCount >= 5)
     {
 //      int rr = random();
 //      if (rr < prob)
@@ -58,6 +101,8 @@ void loop() {
           irRight = irReading(String("r"));
           //delay(500);
           counter = counter + 1;
+          leftCount = 0;
+          rightCount = 0;
         }
 //      }
 //      else
@@ -113,15 +158,44 @@ int irDetect(int irLedPin, int irReceiverPin, long frequency)
 
 int irReading(String leftOrRight){
   int irRead = 0;
+  int freqSensivity = 46000;
   if (leftOrRight=="l")
   {
-    irRead = irDetect(9, 3, 38000) ;
+    irRead = irDetect(9, 3, freqSensivity) ;
 //    Serial.println("LeftReading");
   }
   else
   {
-    irRead = irDetect(5, 2, 38000) ;
+    irRead = irDetect(5, 2, freqSensivity) ;
 //    Serial.println("RightReading");
   }
   return irRead;
+}
+
+void stopMoving(){
+  servoRight.writeMicroseconds(1500);
+  servoLeft.writeMicroseconds(1500);
+}
+
+void foundMine(String leftOrRight){
+  stopMoving();
+  delay(3000);
+  if (leftOrRight=="l")
+  {
+    turnRight();    
+  }
+  else if (leftOrRight=="r"){
+    turnLeft(); 
+  }
+  else{
+    // means both photo transistor find mines
+    turn90degreesRight();
+  }
+  delay(300);
+  goForward();
+}
+
+float volts(int adPin) // Measures volts at adPin
+{ // Returns floating point voltage
+  return float(analogRead(adPin)) * 5.0 / 1024.0;
 }
